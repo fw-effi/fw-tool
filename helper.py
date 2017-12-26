51d18917c91164ef3d63de82c99833e91a80e48d
Andy Scherer <ofee42@gmail.com>
2017-12-27 00:25:49 +0100

    wip

--- helper.py
+++ helper.py
@@ -65,7 +65,7 @@ def lodur_get_appellliste(req_session):
 	html_page = req_session.post('https://lodur-zh.ch/iel/index.php?modul=25&what=339&anz=1', data=post_data).content
 
 	tbl = etree.HTML(html_page).find('//*[@id="mann_tab"]')
-        tbl_rows = iter(tbl)
+	tbl_rows = iter(tbl)
 	tbl_headers = [col.text for col in next(tbl_rows)]
 	for row in rows:
 	    values = [col.text for col in row]
