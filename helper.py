74cce0aa2b30ca0df3db992bba25aa810a316fc7
Andy Scherer <ofee42@gmail.com>
2017-12-26 23:21:39 +0000

    wip

--- helper.py
+++ helper.py
@@ -64,7 +64,7 @@ def lodur_get_appellliste(req_session):
 	# Do the POST request for the table with the information
 	html_page = req_session.post('https://lodur-zh.ch/iel/index.php?modul=25&what=339&anz=1', data=post_data).content
 
-	tbl_tree = html.fromstring(html_page)
+	tbl_tree = html.fromstring(html_page.encode('utf-8'))
         
 	for tbl in tbl_tree.xpath('//*[@id="mann_tab"]'): 
 	    elements = tbl.xpath('.//tbody/tr/td//text()')
