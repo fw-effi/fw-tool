cbfdee872cdeba36307380b4002f9da9b40853d6
Andy Scherer <ofee42@gmail.com>
2017-12-26 17:56:03 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,97 @@
+#!/usr/env/python
+
+import os
+import urllib.parse
+import cookiejar
+import json
+import sys
+import pdfkit
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from helper import *
+
+app = Flask(__name__)
+_session = requests.session()
+_user = None
+
+@app.route("/")
+def home():
+	"""Default Page
+	Check the login state first - if not loggedin redirect it to the login page
+	
+	Keyword Arguments:
+	none
+	"""
+	
+	if not session.get("logged_in"):
+		#If not loggedin redirect it to the login page
+		return render_template("admin/pages/login.html")
+	else:
+		return render_template("admin/index.html", user=_user)
+
+@app.route("/login", methods=['POST'])
+def do_admin_login():
+	""" User send Login information by POST
+	forward it to the helper function (lodur_login) with the Username and Password from the webform
+
+	Keyword Arguments:
+	none
+	"""
+	global _session
+	global _user
+	
+	_session = requests.session()
+	login = lodur_login(request.form['username'],request.form['password'],_session)
+
+	if login['result']:
+		# If the result value from the response array are true, set the session state and go to the homepage
+		session['logged_in'] = True
+		# Save the session variable in the global variable
+		_session = login['session']
+
+		# Save User information in global variable
+		_user = lodur_get_userdata(_session)
+		
+		return home()
+	else:
+		# Are the login failed, give back the following error message - used by the template
+		error="Benutzername oder Passwort falsch!"
+
+	return render_template("/admin/pages/login.html", error=error)
+
+@app.route("/logout", methods=['GET'])
+def do_admin_logout():
+	""" User logout - change session Variable
+	"""
+
+	session['logged_in'] = False
+
+	return render_template("/admin/pages/login.html", error="Logout erfolgreich!")
+
+
+@app.route("/download/excel/alarm_group", methods=['GET'])
+def do_excel_alarm_group():
+	""" Get Excel List with Alarmgroups per Person
+	"""
+
+	if not session.get("logged_in"):
+		#If not loggedin redirect it to the login page
+		return render_template("admin/pages/login.html")
+	else:
+		return render_template("admin/pages/excel_alarm_person.html", user=_user)
+
+@app.route("/test/pdf", methods=['GET'])
+def get_excel_alarm_group():
+
+	if not session.get("logged_in"):
+		#If not loggedin redirect it to the login page
+		return render_template("admin/pages/login.html")
+	else:
+		page = render_template("admin/pages/excel_alarm_person.html", user=_user)
+		pdfkit.from_string(page, 'pdfs/file.pdf')
+		return page
+
+if __name__ == "__main__":
+	app.secret_key = os.urandom(12)
+	app.run(debug=True,host='0.0.0.0',port=80)
+
---
+++ helper.py
@@ -0,0 +1,86 @@
+import requests
+from flask import Response
+from lxml import html
+
+def lodur_login(username,password,sess_login):
+	""" Login User in Lodur. Use the Username and Password from the Webform
+
+	Keyword arguments:
+	username   -- Username from Webform
+	password   -- Password from Webform
+	sess_login -- global variable for requests session information
+	"""
+
+	# Url with the Login Form - used to get the first PHPSESSID Cookie
+	url_form = "https://lodur-zh.ch/iel/index.php?modul=9"
+	# Url for the POST Request with the form data
+	url_login = "https://lodur-zh.ch/iel/index.php"
+	# Create object with login data (url encoding automaticly by python)
+	form_data = { "login_member_name": username, "login_member_pwd": password }
+
+	#request the login from - genereate PHPSESSID cookie
+	req_form = sess_login.get(url_form)
+	# save the PHPSESSID Cookie bevor we do the login
+	phpsess_bevor = requests.utils.dict_from_cookiejar(sess_login.cookies)
+
+	#do the login (POST Request)
+	req_login = sess_login.post(url_login, data=form_data, headers={'User-Agent':'Mozilla/5.0'})
+	#save the PHPSESSID Cookie after the login (it should be different from the first one)
+	phpsess_after = requests.utils.dict_from_cookiejar(sess_login.cookies)
+
+	# create array with the needed information for the return command
+	result = {"result": not phpsess_bevor == phpsess_after, "session": sess_login}
+	
+	return result
+
+def lodur_list_alarm(req_session):
+	""" Get the excel list from Lodur used for the 'Appellblaetter'
+
+	Keyword arguments:
+	req_session -- requests session variable with the PHPSESSID from the login
+	"""
+
+	print(requests.utils.dict_from_cookiejar(req_session.cookies))
+
+	# Create object for the POST Request
+	post_data = {
+		"status": 1,
+		"mannschaftslisten_info_adf_0": -1,
+		"mannschaftslisten_info_adf_1": -1,
+		"mannschaftslisten_info_adf_2": -1,
+		"gruppe_0": -1,
+		"zug_0": -1,
+		"mannschaftslisten_info_field_sel_0_0":104,
+		"mannschaftslisten_info_field_sel_1_0":33,
+		"mannschaftslisten_info_field_sel_2_0":103,
+		"mannschaftslisten_info_field_sel_3_0":86,
+		"rows":1,
+		"cols":4,
+		"adfs":3,
+		"gruppes":"1",
+		"zugs":1,
+	}
+	
+	# Do the POST request for the table with the information
+	req_data = req_session.post('https://lodur-zh.ch/iel/xlsgateway.php?modul=25&what=353&xls=1&format=1', data=post_data, headers={'User-Agent':'Mozilla/5.0'}, stream=True)
+
+	res = Response(req_data.raw)
+	res.headers['Content-Disposition'] = 'attachment; filename=alarmliste.txt'
+	res.mimetype='application/vnd.ms-excel; charset: ISO-8859-1'
+	
+	return res
+        
+def lodur_get_userdata(req_session):
+	""" Get the Userdata (like First-, Lastname and E-Mail Address) from the Lodur start page
+
+	Keyword arguments:
+	req_session -- requests session variable with the PHPSESSID from the login
+	"""
+
+	html_page = req_session.get('https://lodur-zh.ch/iel').content
+
+	tbl_tree = html.fromstring(html_page)
+	name = tbl_tree.xpath('//*[@id="adf_info_tbl"]/tbody/tr[1]/td/text()')
+	mail = tbl_tree.xpath('//*[@id="adf_info_tbl"]/tbody/tr[4]/td/span[2]/text()')
+
+	return { 'name': name[0], 'mail': mail[0] }
