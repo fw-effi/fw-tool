ef50406f3c552e9847f428fafd6f215072f493de
Andy Scherer <ofee42@gmail.com>
2017-12-28 00:51:20 +0000

    wip

--- helper.py
+++ helper.py
@@ -115,7 +115,7 @@ def lodur_get_appellliste(req_session):
 	        result["va"].append({"grad":grad,"name":name,"vorname":vorname})
 	    if 'ADL' in gruppe:
 	        result["adl"].append({"grad":grad,"name":name,"vorname":vorname})
-	    if u'Sanit‰tsabteilung' in gruppe:
+	    if 'Sanit√§tsabteilung' in gruppe:
 	        result["san"].append({"grad":grad,"name":name,"vorname":vorname})
 
 	return result
