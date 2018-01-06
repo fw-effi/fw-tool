e07664eabd2c43cf650c51d7014ea3be476c6f81
Andy Scherer <ofee42@gmail.com>
2018-01-06 21:22:17 +0000

    wip

--- helperPDF.py
+++ helperPDF.py
@@ -16,9 +16,9 @@ def pdf_get_alarmgruppe(gruppe):
 		'ka1': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka1"],gruppe = "KA 1"),
 		'ka2': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka2"],gruppe = "KA 2"),
 		'ka3': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka3"],gruppe = "KA 3"),
-		'ka4': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka3"],gruppe = "KA 4"),
-		'ka5': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka3"],gruppe = "KA 5"),
-		'ka6': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka3"],gruppe = "KA 6"),
+		'ka4': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka4"],gruppe = "KA 4"),
+		'ka5': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka5"],gruppe = "KA 5"),
+		'ka6': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["ka6"],gruppe = "KA 6"),
 		'bag': render_template("pdf/liste-alarmgruppe-bag.html",adfs = pdfcontent["bag"],gruppe = "Bagatell"),
 		'all': render_template("pdf/liste-alarmgruppe-ka.html",adfs = pdfcontent["all"],gruppe = "Alle")
 	    }[gruppe]
--- templates/AdminBSBMaterialDesign-1.0.5/js/service-push.js
+++ templates/AdminBSBMaterialDesign-1.0.5/js/service-push.js
@@ -1,12 +1,64 @@
-self.addEventListener('install', function() {
-  self.skipWaiting();
+self.addEventListener('push', function(event) {
+    if (!(self.Notification && self.Notification.permission === 'granted')) {
+        return;
+    }
+
+    var data = {};
+    if (event.data) {
+        data = event.data.json();
+    }
+    console.log(data);
+    var title = data.title;
+    var message = data.message;
+    var icon = "img/FM_logo_2013.png";
+    var actions = [];
+
+    self.clickTarget = data.clickTarget;
+    if(data.clickMap) {
+      self.clickMap = data.clickMap;
+      actions= [
+          {
+            action: 'googleMaps',
+            title: 'Karte anzeigen',
+            icon: ''
+          }
+        ];
+    }
+
+    event.waitUntil(self.registration.showNotification(title, {
+        body: message,
+        tag: 'push-demo',
+        icon: icon,
+        badge: icon,
+        actions: actions
+    }));
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(clients.claim());
+/**** START notificationActionClickEvent ****/
+self.addEventListener('notificationclick', function(event) {
+  if (!event.action) {
+    // Was a normal notification click
+    console.log('Notification Click.');
+    return;
+  }
+
+  switch (event.action) {
+    case 'googleMaps':
+      event.waitUntil(clients.openWindow(self.clickTarget));
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
 });
+/**** END notificationActionClickEvent ****/
 
 self.addEventListener('notificationclick', function(event) {
-  // Close the notification when it is clicked
-  event.notification.close();
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    if(clients.openWindow){
+        event.waitUntil(clients.openWindow(self.clickTarget));
+    }
 });
