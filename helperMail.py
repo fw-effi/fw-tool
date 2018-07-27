d02fa364428686989bbdb37fe4fdc32a3c52af3b
Andy Scherer <ofee42@gmail.com>
2018-07-27 18:27:04 +0200

    wip

--- helperMail.py
+++ helperMail.py
@@ -20,9 +20,11 @@ def mail_post_sendOneSmtp(request,user):
     message = MIMEMultipart()
     message['From'] = "%s <admin@scherer.me>" % user['name'].split(" ",1)[1]
     message['To']= request.form['mailTo']
+    message.add_header('Reply-To',user['mail'].split("|",1)[0])
     message['Subject'] = request.form['mailSubject']
-    #message.attach(MIMEText(remove_html_tags(request.form['mailBody']),'plain'))
-    message.attach(MIMEText(request.form['mailBody'],'html'))
+    message.attach(MIMEText("<html>%s</html>" % request.form['mailBody'], 'html'))
+    message.attach(MIMEText(remove_html_tags(request.form['mailBody']),'plain'))
+
 
     server = smtplib.SMTP('smtp.migadu.com',587)
     server.starttls()
